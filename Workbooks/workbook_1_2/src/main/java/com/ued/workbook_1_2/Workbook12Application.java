10ef8dafedfe1025eee2e3b180642c792a0b5d6b
Ugur Dogan <ugur.dogan@resmed.com>
2022-06-24 19:01:37 -0700

    ECO-54465: Workbook 1.2 work added.

---
+++ Workbooks/workbook_1_2/src/main/java/com/ued/workbook_1_2/Workbook12Application.java
@@ -0,0 +1,13 @@
+package com.ued.workbook_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workbook12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workbook12Application.class, args);
+	}
+
+}
---
+++ Workbooks/workbook_1_2/src/test/java/com/ued/workbook_1_2/Workbook12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ued.workbook_1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workbook12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
