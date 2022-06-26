d4b48ec739751ee89fa88a6cb945097429fd039d
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-06-26 15:36:27 -0700

    Workbook 2.6 solved.

--- 2. model-view-controller/workbook 2.6/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 2. model-view-controller/workbook 2.6/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -9,6 +9,8 @@ public class WorkbookController {
 
     @GetMapping(value="/")
     public String getMethodName(Model model) {
+        model.addAttribute("budget", 6000);
+        model.addAttribute("make", "BMW");
         return "dealership";
     }
     
