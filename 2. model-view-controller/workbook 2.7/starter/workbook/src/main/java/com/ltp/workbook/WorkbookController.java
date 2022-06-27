1f0240088bde588f9da347e3a931cce04832f429
Ugur Emre Dogan <ue.dogan@gmail.com>
2022-06-26 20:24:16 -0700

    Workbook 2.7 solved.

--- 2. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 2. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.sym.Name;
+
 @Controller
 public class WorkbookController {
     
     @GetMapping("/")
     public String getName(Model model) {
+        model.addAttribute("name", "Ugur");
         return "name-list";
     }
 }
