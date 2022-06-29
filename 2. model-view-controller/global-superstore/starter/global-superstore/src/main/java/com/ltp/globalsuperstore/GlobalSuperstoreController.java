1dbcc953cf9e3deb6888d9fee6a23338f5bdcd31
Ugur Dogan <ugur.dogan@resmed.com>
2022-06-29 12:01:44 -0700

    Challenge 2: Part 2 solved

--- 2. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
+++ 2. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreController.java
@@ -3,6 +3,7 @@ package com.ltp.globalsuperstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,16 +12,24 @@ import java.util.List;
 
 @Controller
 public class GlobalSuperstoreController {
+    List<Item> items =  new ArrayList<>();
 
     @GetMapping("/")
     public String getForm(Model model) {
-
         model.addAttribute("categories",Constants.CATEGORIES);
+        model.addAttribute("item", new Item());
         return "form";
     }
 
     @GetMapping("/inventory")
     public String getInventory(Model model){
+        model.addAttribute("items",items);
         return "inventory";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitItem(Item item){
+        items.add(item);
+        return "redirect:/inventory";
     }
 }
